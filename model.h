eff6d3253157082843d4f5359add7c0722ac4c01
SashaM69420 <alex.n.martynov@gmail.com>
2020-04-08 11:44:27 -0400

    Created a wrapper class for 3d models

--- model.h
+++ model.h
@@ -1,8 +1,24 @@
 #pragma once
+
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <glm/glm.hpp>
+
 class model
 {
+private:
+	std::vector<glm::vec3> vertices = std::vector<glm::vec3>();
+	std::vector<glm::vec3> colors = std::vector<glm::vec3>();
+	std::vector<glm::vec3> normals = std::vector<glm::vec3>();
+
 public:
 	model();
+	model(const model& m);
 	~model();
+
+	bool load_model(const char * obj_path, const char * mtl_path);
+	void translate(double x, double y, double z);
 };
 
