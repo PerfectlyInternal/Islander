b379f14ecdea85dfa0a9edcf46aa293074a4df85
SashaM69420 <alex.n.martynov@gmail.com>
2020-04-09 11:10:20 -0400

    Added the tree model

--- include/internal/terrain_generation.cpp
+++ include/internal/terrain_generation.cpp
@@ -328,7 +328,7 @@ void generate_terrain(int size, int iterations, double amplitude, std::vector<st
 			if (map[i * tree_freq + x][j * tree_freq + z] > water_level)
 			{
 				model tree = model();
-				tree.load_model("untitled.obj", "untitled.mtl");
+				tree.load_model("tree.obj", "tree.mtl");
 				tree.translate(i * tree_freq + x - size/2, map[i * tree_freq + x][j * tree_freq + z], j * tree_freq + z - size/2);
 				tree.get_model(trees, colors, normals);
 			}
