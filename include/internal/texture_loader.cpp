c900b65f3edfbaeed3c855d4349f9d26fa286f1d
SashaM69420 <alex.n.martynov@gmail.com>
2020-04-12 19:08:24 -0400

    Cleaning up and adding at text renderer

--- include/internal/texture_loader.cpp
+++ include/internal/texture_loader.cpp
@@ -139,20 +139,20 @@ GLuint load_dds(const char * imagepath) {
 	}
 
 	// Create one OpenGL texture
-	GLuint textureID;
-	glGenTextures(1, &textureID);
+	GLuint texture_id;
+	glGenTextures(1, &texture_id);
 
 	// "Bind" the newly created texture : all future texture functions will modify this texture
-	glBindTexture(GL_TEXTURE_2D, textureID);
+	glBindTexture(GL_TEXTURE_2D, texture_id);
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
-	unsigned int blockSize = (format == GL_COMPRESSED_RGBA_S3TC_DXT1_EXT) ? 8 : 16;
+	unsigned int block_size = (format == GL_COMPRESSED_RGBA_S3TC_DXT1_EXT) ? 8 : 16;
 	unsigned int offset = 0;
 
 	/* load the mipmaps */
 	for (unsigned int level = 0; level < mipmap_count && (width || height); ++level)
 	{
-		unsigned int size = ((width + 3) / 4)*((height + 3) / 4)*blockSize;
+		unsigned int size = ((width + 3) / 4) * ((height + 3) / 4) * block_size;
 		glCompressedTexImage2D(GL_TEXTURE_2D, level, format, width, height,	0, size, buffer + offset);
 
 		offset += size;
@@ -166,5 +166,5 @@ GLuint load_dds(const char * imagepath) {
 
 	free(buffer);
 
-	return textureID;
+	return texture_id;
 }
